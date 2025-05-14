99fae68ab222192afa475d93786a347d1d5bb1ec
vidura002 <viduransilu2002@gmail.com>
2025-05-14 15:33:29 +0530

    fix errors and update the code in post controller

--- BACKEND/src/main/java/com/example/Backend/controller/PostController.java
+++ BACKEND/src/main/java/com/example/Backend/controller/PostController.java
@@ -92,7 +92,7 @@ public class PostController {
     }
 
  
-    @PostMapping("/{postId}/like")
+    @PostMapping("/{postId}/likes")
     public ResponseEntity<Post> addLike(@PathVariable String postId, @RequestBody Like like) {
         Post updatedPost = postService.addLike(postId, like);
         return new ResponseEntity<>(updatedPost, HttpStatus.CREATED);
