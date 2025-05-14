6ff3f34ef35606ccee78bd5bc7df529089793e32
vidura002 <viduransilu2002@gmail.com>
2025-05-14 15:30:18 +0530

    create Auth.jsx file

--- FRONTEND/src/context/auth/AuthContext.js
+++ FRONTEND/src/context/auth/AuthContext.js
@@ -1,11 +1,1 @@
-import { createContext } from 'react';
 
-const AuthContext = createContext({
-  currentUser: null,
-  login: () => {},
-  logout: () => {},
-  updateUser: () => {},
-  loading: true
-});
-
-export default AuthContext;
